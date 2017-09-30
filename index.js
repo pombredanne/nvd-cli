2c1f7c4248eefe7c4d6c9074a2c4e4b07500479a
Lilith Tundrus <echofox123@gmail.com>
2017-09-29 23:44:16 -0400

    started adding framework to index.js, added an empty config.js file to be used later

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 'use strict';
+const request = require('request');                             //for NVD API calls
+const rp = require('request-promise');                          //wrap request with promises for easir flow control
+const fs = require('fs');                                       //for reading the JSON file
+
 
 //simple script to get recent NVD JSON data from their CDN in a zip format
 //unzip it and do some stuff using past project's code
+
+//TODO:
+//Allow for vulerability severity configuration based on the config.js file
+
+//script starts here
+Promise.resolve()                                               //start the promise chain as resolved to avoid issues
+.then(() => {
+    console.log(`\nNVD Recent Vulnerability Script Started on ${new Date().toISOString()}`);
+})
+.then(() => {
+    //Get the RECENT json
+})
