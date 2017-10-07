bba9b690edcb6f181f1f88fafdd4783e89b75eb4
Lilith Tundrus <echofox123@gmail.com>
2017-10-07 15:47:06 -0400

    updated config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ exports.NVDPath = 'NVDJSON/';
 //choose between recent, or any other NVD .ZIP/.GZ type
 exports.NVDURL = 'https://static.nvd.nist.gov/feeds/json/cve/1.0/nvdcve-1.0-2017.json.zip';
 exports.compressedExtension = '.zip'
-exports.NVDJSONFileName = 'nvdcve-1.0-2017.json';
+exports.NVDJSONFileName = './nvdcve-1.0-2017.json';
