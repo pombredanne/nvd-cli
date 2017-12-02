e31374020de5f6971e5d09ce045403afd4dced36
Lilith Tundrus <echofox123@gmail.com>
2017-12-01 20:16:33 -0500

    added zip file location var for modularity

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ exports.NVDPath = 'NVDJSON/';
 //choose between recent, or any other NVD .ZIP/.GZ type
 exports.NVDURL = 'https://static.nvd.nist.gov/feeds/json/cve/1.0/nvdcve-1.0-recent.json.zip';
 exports.compressedExtension = '.zip';
+exports.zipFileName = './nvdzip.zip'
 exports.NVDJSONFileName = './nvdcve-1.0-recent.json';
 exports.checklistName = './checklist.json';
 exports.defaultFontLocation = './fonts/Raleway-Regular.ttf';
