d063bf1e9ef83675267de6e5e31dc4c9d4f8d5d7
Lilith Tundrus <echofox123@gmail.com>
2017-12-01 20:24:59 -0500

    moved each promise section to a function where it made sense. Reduced code slightly

--- index.js
+++ index.js
@@ -26,123 +26,6 @@ TODO: fix the weird promise chain thing and chunk functions out
 TODO: when done, work on README
 */
 
-// script starts here
-console.log(`\nNVD RECENT Vulnerability Script Started on ${new Date().toISOString()}\n`);
-Promise.resolve()                                               // start the promise chain as resolved to avoid issues
-    .then(() => {                                               // Get the RECENT json that is in .zip format
-        return getNVDZipFile();
-    })
-    .then(() => {
-        return extractZipFile(config.zipFileName);
-    })
-    .then(() => {
-        // read file contents to memory
-        let NVDJSON = fs.readFileSync(config.NVDJSONFileName, 'utf-8');
-        let parsedNVDData = JSON.parse(NVDJSON);
-        globalNVDJSON = parsedNVDData;                              // used to allow the PDF file acess to certain data
-        return parsedNVDData;
-    })
-    .then((NVDObj) => {
-        console.log(`CVE data version: ${NVDObj.CVE_data_version}`);
-        console.log(`CVE count: ${NVDObj.CVE_data_numberOfCVEs}`);
-        console.log(`Last Updated: ${NVDObj.CVE_data_timestamp}`);
-        var affectedItems = [];
-        NVDObj.CVE_Items.forEach((entry, index) => {
-            var affectedItem = {};
-            entry.cve.affects.vendor.vendor_data.forEach((entryV, indexV) => {
-                // check against the list of vendors to check for vulnerabilities
-                swChecklist.forEach((item, itemIndex) => {
-                    if (entryV.vendor_name.toLowerCase() == item.manufacturerName.toLowerCase()) {
-                        entryV.product.product_data.forEach((product, productIndex) => {
-                            if (product.product_name == item.softwareName.toLowerCase()) {
-                                console.log(entry)
-                                var versionsAffected = [];
-                                entryV.product.product_data[0].version.version_data.forEach((version) => {
-                                    versionsAffected.push(version.version_value);
-                                });
-                                // push all of the data to an the affectedItem Obj
-                                affectedItem.ID = entry.cve.CVE_data_meta.ID;
-                                affectedItem.vendorName = entryV.vendor_name;
-                                affectedItem.productName = entryV.product.product_data[0].product_name;
-                                affectedItem.publishedDate = entry.publishedDate;
-                                affectedItem.lastModifiedDate = entry.lastModifiedDate;
-                                affectedItem.vulnerabilityDescription = entry.cve.description.description_data[0].value;
-                                affectedItem.versionsAffected = versionsAffected;
-                                // validate that v3 exists
-                                if (entry.impact.hasOwnProperty('baseMetricV3')) {
-                                    affectedItem.v3SeverityScore = {
-                                        severity: entry.impact.baseMetricV3.cvssV3.baseSeverity,
-                                        scoreString: entry.impact.baseMetricV3.cvssV3.baseScore
-                                    }
-                                    affectedItem.attackVector = entry.impact.baseMetricV3.cvssV3.attackVector;
-                                } else {
-                                    affectedItem.v3SeverityScore = {
-                                        severity: 'NONE',
-                                        scoreString: 'NONE'
-                                    }
-                                }
-                                // Do the same for v2
-                                if (entry.impact.hasOwnProperty('baseMetricV2')) {
-                                    affectedItem.v2SeverityScore = {
-                                        severity: entry.impact.baseMetricV2.severity,
-                                        scoreString: entry.impact.baseMetricV2.cvssV2.baseScore
-                                    }
-                                } else {
-                                    affectedItem.v2SeverityScore = {
-                                        severity: 'NONE',
-                                        scoreString: 'NONE'
-                                    }
-                                }
-                                // push the affected item to the array to return
-                                affectedItems.push(affectedItem);
-                            }
-                        });
-                    }
-                });
-            });
-        });
-        return affectedItems;
-    })
-    .then((affectedItemsArray) => {
-        // write the report to PDF
-        var doc = new PDFDocument;
-        doc.pipe(fs.createWriteStream('output.pdf'));
-        doc.fontSize(16);
-        doc.font(config.defaultFontLocation);
-        doc.text(`NVD RECENT Vulnerability Check Report ${new Date().toDateString()}`, { align: 'center', stroke: true });
-        doc.fontSize(12);
-        doc.moveDown();
-        doc.moveDown();
-        doc.text(`CVE data version: ${globalNVDJSON.CVE_data_version}`);
-        doc.text(`CVE count: ${globalNVDJSON.CVE_data_numberOfCVEs}`);
-        doc.text(`Last Updated: ${globalNVDJSON.CVE_data_timestamp}`);
-        doc.moveDown();
-        doc.fontSize(14);
-        doc.text(`RECENT vulnerabilites matched using config file: ${config.checklistName}`, { align: 'center' });
-        doc.moveDown();
-        // get each affected item's data and format it
-        affectedItemsArray.forEach((entry, index) => {
-            console.log(entry);
-            doc.text(`\n${capitalizeFirstLetter(entry.vendorName)} ${capitalizeFirstLetter(entry.productName)} (${entry.ID})`, { stroke: true });
-            doc.text(`Versions Affected: ${entry.versionsAffected.join(', ')}`);
-            doc.text(`Attack Vector: ${entry.attackVector}`);
-            doc.text(`\nDescription: ${entry.vulnerabilityDescription}`);
-            doc.text(`\nV3 Score: ${entry.v3SeverityScore.severity} (${entry.v3SeverityScore.scoreString})`);
-            doc.text(`V2 Score: ${entry.v2SeverityScore.severity} (${entry.v2SeverityScore.scoreString})`);
-            doc.text(`\nPublished: ${entry.publishedDate}    Modified: ${entry.lastModifiedDate}`)
-        });
-
-        doc.text('\nEnd of File');
-        doc.end();
-
-    })
-    .then(() => {
-        console.log(`\nSuccessfully ended on ${new Date().toISOString()}`);
-    })
-    .catch((err) => {
-        console.log(`Ended with error at ${new Date().toISOString()}: ${err}`);
-    });
-
 
 function capitalizeFirstLetter(string) {                    //used to clean up some WF data 
     return string.charAt(0).toUpperCase() + string.slice(1);
@@ -166,3 +49,123 @@ function extractZipFile(fileNameToExtract) {
         if (err) { return console.log(err); }                                              // extraction is complete,  make sure to handle the err 
     });
 }
+
+function parseNVDData(NVDObjArray) {
+    console.log(`CVE data version: ${NVDObjArray.CVE_data_version}`);
+    console.log(`CVE count: ${NVDObjArray.CVE_data_numberOfCVEs}`);
+    console.log(`Last Updated: ${NVDObjArray.CVE_data_timestamp}`);
+    var affectedItems = [];
+    NVDObjArray.CVE_Items.forEach((entry, index) => {
+        var affectedItem = {};
+        entry.cve.affects.vendor.vendor_data.forEach((entryV, indexV) => {
+            // check against the list of vendors to check for vulnerabilities
+            swChecklist.forEach((item, itemIndex) => {
+                if (entryV.vendor_name.toLowerCase() == item.manufacturerName.toLowerCase()) {
+                    entryV.product.product_data.forEach((product, productIndex) => {
+                        if (product.product_name == item.softwareName.toLowerCase()) {
+                            console.log(entry)
+                            var versionsAffected = [];
+                            entryV.product.product_data[0].version.version_data.forEach((version) => {
+                                versionsAffected.push(version.version_value);
+                            });
+                            // push all of the data to an the affectedItem Obj
+                            affectedItem.ID = entry.cve.CVE_data_meta.ID;
+                            affectedItem.vendorName = entryV.vendor_name;
+                            affectedItem.productName = entryV.product.product_data[0].product_name;
+                            affectedItem.publishedDate = entry.publishedDate;
+                            affectedItem.lastModifiedDate = entry.lastModifiedDate;
+                            affectedItem.vulnerabilityDescription = entry.cve.description.description_data[0].value;
+                            affectedItem.versionsAffected = versionsAffected;
+                            // validate that v3 exists
+                            if (entry.impact.hasOwnProperty('baseMetricV3')) {
+                                affectedItem.v3SeverityScore = {
+                                    severity: entry.impact.baseMetricV3.cvssV3.baseSeverity,
+                                    scoreString: entry.impact.baseMetricV3.cvssV3.baseScore
+                                }
+                                affectedItem.attackVector = entry.impact.baseMetricV3.cvssV3.attackVector;
+                            } else {
+                                affectedItem.v3SeverityScore = {
+                                    severity: 'NONE',
+                                    scoreString: 'NONE'
+                                }
+                            }
+                            // Do the same for v2
+                            if (entry.impact.hasOwnProperty('baseMetricV2')) {
+                                affectedItem.v2SeverityScore = {
+                                    severity: entry.impact.baseMetricV2.severity,
+                                    scoreString: entry.impact.baseMetricV2.cvssV2.baseScore
+                                }
+                            } else {
+                                affectedItem.v2SeverityScore = {
+                                    severity: 'NONE',
+                                    scoreString: 'NONE'
+                                }
+                            }
+                            // push the affected item to the array to return
+                            affectedItems.push(affectedItem);
+                        }
+                    });
+                }
+            });
+        });
+    });
+    return affectedItems;
+}
+
+function writePDFReport(affectedItemsArray) {
+    // write the report to PDF
+    var doc = new PDFDocument;
+    doc.pipe(fs.createWriteStream('output.pdf'));
+    doc.fontSize(16);
+    doc.font(config.defaultFontLocation);
+    doc.text(`NVD RECENT Vulnerability Check Report ${new Date().toDateString()}`, { align: 'center', stroke: true });
+    doc.fontSize(12);
+    doc.moveDown();
+    doc.moveDown();
+    doc.text(`CVE data version: ${globalNVDJSON.CVE_data_version}`);
+    doc.text(`CVE count: ${globalNVDJSON.CVE_data_numberOfCVEs}`);
+    doc.text(`Last Updated: ${globalNVDJSON.CVE_data_timestamp}`);
+    doc.moveDown();
+    doc.fontSize(14);
+    doc.text(`RECENT vulnerabilites matched using config file: ${config.checklistName}`, { align: 'center' });
+    doc.moveDown();
+    // get each affected item's data and format it
+    affectedItemsArray.forEach((entry, index) => {
+        console.log(entry);
+        doc.text(`\n${capitalizeFirstLetter(entry.vendorName)} ${capitalizeFirstLetter(entry.productName)} (${entry.ID})`, { stroke: true });
+        doc.text(`Versions Affected: ${entry.versionsAffected.join(', ')}`);
+        doc.text(`Attack Vector: ${entry.attackVector}`);
+        doc.text(`\nDescription: ${entry.vulnerabilityDescription}`);
+        doc.text(`\nV3 Score: ${entry.v3SeverityScore.severity} (${entry.v3SeverityScore.scoreString})`);
+        doc.text(`V2 Score: ${entry.v2SeverityScore.severity} (${entry.v2SeverityScore.scoreString})`);
+        doc.text(`\nPublished: ${entry.publishedDate}    Modified: ${entry.lastModifiedDate}`)
+    });
+
+    doc.text('\nEnd of File');
+    doc.end();
+
+}
+
+// script starts here
+// args will eventually be processed here
+console.log(`\nNVD RECENT Vulnerability Script Started on ${new Date().toISOString()}\n`);
+Promise.resolve()                                               // start the promise chain as resolved to avoid issues
+    .then(() => getNVDZipFile())                                // Get the RECENT json that is in .zip format
+    .then(() => extractZipFile(config.zipFileName))
+    .then(() => {
+        // read file contents to memory
+        let NVDJSON = fs.readFileSync(config.NVDJSONFileName, 'utf-8');
+        let parsedNVDData = JSON.parse(NVDJSON);
+        globalNVDJSON = parsedNVDData;                              // used to allow the PDF file acess to certain data
+        return parsedNVDData;
+    })
+    .then((NVDData) => parseNVDData(NVDData))
+    .then((affectedItemsArray) => {
+        writePDFReport(affectedItemsArray)
+    })
+    .then(() => {
+        console.log(`\nSuccessfully ended on ${new Date().toISOString()}`);
+    })
+    .catch((err) => {
+        console.log(`Ended with error at ${new Date().toISOString()}: ${err}`);
+    })
