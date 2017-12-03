40a822744fc207a8627891af7f6edb0b701ecdc9
Lilith Tundrus <echofox123@gmail.com>
2017-12-02 21:00:16 -0500

    rewmoved hard coded 2017 exports

--- config.js
+++ config.js
@@ -5,11 +5,6 @@ exports.compressedExtension = '.zip';
 exports.zipFileNameRecent = './nvdzipRecent.zip'
 exports.NVDJSONFileNameRecent = './nvdcve-1.0-recent.json';
 
-exports.NVDURLFull2017 = 'https://static.nvd.nist.gov/feeds/json/cve/1.0/nvdcve-1.0-2017.json.zip';
-exports.zipFileNameFUll2017 = './nvdfullzip2017.zip';
-exports.NVDJSONFileNameFull2017 = './nvdcve-1.0-2017.json';
-
-
 exports.checklistName = './checklist.json';
 exports.defaultFontLocation = './fonts/Raleway-Regular.ttf';
-exports.debug = true;
+exports.debug = false;
